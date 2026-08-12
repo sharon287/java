de6b429fa0e97f1a784d8ee6105564360db22907
sharon287 <sharonabraham2029@cs.sjcetpalai.ac.in>
2026-08-12 18:46:03 +0530

    Create max.java

---
+++ tutorial5./max.java
@@ -0,0 +1,27 @@
+import java.util.Vector;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Vector<Integer> numbers = new Vector<>();
+
+        numbers.add(25);
+        numbers.add(60);
+        numbers.add(15);
+        numbers.add(80);
+        numbers.add(45);
+
+        int max = numbers.get(0);
+
+        for(int number : numbers)
+        {
+            if(number > max)
+            {
+                max = number;
+            }
+        }
+
+        System.out.println("Largest number = " + max);
+    }
+}
