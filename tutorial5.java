ddf7a891291ae8717ae84cdcd25662b0e944710d
sharon287 <sharonabraham2029@cs.sjcetpalai.ac.in>
2026-08-12 18:39:32 +0530

    Create tutorial5.java

---
+++ tutorial5.java
@@ -0,0 +1,21 @@
+package mod1;
+import java.util.Vector;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Vector<String> fruits = new Vector<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+        fruits.add("Orange");
+        fruits.add("Grapes");
+
+        for(String fruit : fruits)
+        {
+            System.out.println(fruit);
+        }
+    }
+}
